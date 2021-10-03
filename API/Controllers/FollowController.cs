ece82ccbc1dd85d652bee663d5ab083e55839422
youngalx2 <89937900+youngalx2@users.noreply.github.com>
2021-10-03 00:40:22 -0700

    add the controller

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -11,11 +11,5 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
         }
-
-        [HttpGet("{username}")]
-        public async Task<IActionResult> GetFollowings(string username, string predicate)
-        {
-            return HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
-        }
     }
 }
