ba757d5e1dc82e14eeb2739f6ee7d4bd95a2d27e
youngalx2 <89937900+youngalx2@users.noreply.github.com>
2021-10-10 02:31:03 -0700

    adding client side pagination

--- client-app/src/app/models/pagination.ts
+++ client-app/src/app/models/pagination.ts
@@ -13,14 +13,4 @@ export class PaginatedResult<T> {
         this.data = data;
         this.pagination = pagination;
     }
-
-}
-export class PagingParams {
-    pageNumber;
-    pageSize;
-
-    constructor(pageNumber = 1, pageSize = 2) {
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-    }
 }
